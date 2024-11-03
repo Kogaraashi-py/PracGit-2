cdeadc69ab5b3ae5c013e0bb13ce629516e67d1a
Kogaraashi-py <hernandoplazadavid@gmail.com>
2024-11-03 23:46:30 +0000

    modifique el move y agrege un fragmento de texto

--- Move.py
+++ Move.py
@@ -5,3 +5,10 @@ x = "mi primer commit"
 print(x)
 print(x)
 
+#ok un vez agregado el readme junto con este archivo para crear su punto de remerencia
+
+y = "segundo commit"
+
+z= "que respiro el aire que respiro el aire que respiras tu"
+
+print(z)
