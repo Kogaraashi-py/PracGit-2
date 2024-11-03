1e9e3242652cf53a2f5c2f3fb1b6d262330e105e
Kogaraashi-py <hernandoplazadavid@gmail.com>
2024-11-03 23:43:18 +0000

    agrege el readmi y move

---
+++ Move.py
@@ -0,0 +1,7 @@
+#en esta ocacion vamos a probar regresar entre commits
+
+x = "mi primer commit"
+
+print(x)
+print(x)
+
---
+++ README.md
@@ -0,0 +1,1 @@
+vamos a probar moviendonos entre ramas y devolviendolas a sus commit anteriores
